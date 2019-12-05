3528073c5e2762ee8efd300cc54b5c484fc699b5
truco23 <trucopaulista23@gmail.com>
2019-12-05 17:56:44 -0300

    Inciando o cadastro de curso

--- src/app/modules/courses/add-courses/add-courses.component.ts
+++ src/app/modules/courses/add-courses/add-courses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CategoriesService } from '../../categories/services/categories.service';
+import { CategoriesInterface } from '../../categories/typings/categories-typing.interface';
 
 @Component({
   selector: 'app-add-courses',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCoursesComponent implements OnInit {
 
-  constructor() { }
+  formAdd: FormGroup
+  categories: CategoriesInterface[]
+  data: any
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _categorieService: CategoriesService
+  ) { }
 
   ngOnInit() {
+
+    this._categorieService
+      .get()
+      .subscribe(data => {
+        
+        this.data = data  
+        this.categories = this.data.docs; 
+        console.log(this.categories);
+      }, error => console.error(error))
+
+    this.formAdd = this._formBuilder.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4)
+        ]
+      ],
+      description: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(50)
+        ]
+      ]
+    })
   }
 
+  create(e: Event): void {
+
+    e.preventDefault()
+
+    const data = this.formAdd.getRawValue()
+
+    console.log('data', data);
+    
+  }
 }
--- src/app/modules/courses/add-courses/add-courses.module.ts
+++ src/app/modules/courses/add-courses/add-courses.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AddCoursesComponent } from './add-courses.component';
+import { JumbotronModule } from '../../shared/jumbotron/jumbotron.module';
+import { MessageModule } from '../../shared/message/message.module';
 import { AddCoursesRoutingModule } from './add-courses.routing.module';
 
 @NgModule({
   declarations: [AddCoursesComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    JumbotronModule,
+    MessageModule,
     AddCoursesRoutingModule
   ],
   exports: [AddCoursesComponent]
