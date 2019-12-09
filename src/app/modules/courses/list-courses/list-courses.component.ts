6a877de6229a34b5d36628d44e78bfcb4a92b98a
truco23 <trucopaulista23@gmail.com>
2019-12-09 16:26:04 -0300

    Implementando o filtro para cursos

--- src/app/modules/courses/list-courses/list-courses.component.ts
+++ src/app/modules/courses/list-courses/list-courses.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { CourseService } from '../services/course.service';
 import { CourseInterface } from '../typings/courses-typing.interface';
+import { CategoriesService } from '../../categories/services/categories.service';
+import { CategoriesInterface } from '../../categories/typings/categories-typing.interface';
 
 @Component({
   selector: 'app-list-courses',
@@ -15,9 +17,11 @@ export class ListCoursesComponent implements OnInit {
   messageSuccess: string
   messageDanger: string
   pagesNumber = []
+  categories: CategoriesInterface[]
 
   constructor(
-    private _courseService: CourseService
+    private _courseService: CourseService,
+    private _categoryService: CategoriesService
   ) { }
 
   ngOnInit() {
@@ -27,17 +31,19 @@ export class ListCoursesComponent implements OnInit {
       .subscribe(data => {
 
         this.data = data
-        this.courses = this.data.result
+        this.courses = this.data.result.docs
         
         for (let i = 1; i < this.data.result.totalPages + 1; i++) {
           this.pagesNumber.push({ page: i })
         }
-        
-        console.log('cursos',this.courses);
-        console.log('paginas', this.pagesNumber);
-        
-        
       }, error => console.log(error))
+
+    this._categoryService
+      .getAll()
+      .subscribe(data => 
+        this.categories = data, 
+        error => console.error(error)
+      )
   }
 
   pagination(page: number) {
@@ -71,5 +77,26 @@ export class ListCoursesComponent implements OnInit {
           this.messageDanger = 'Não foi possível remover a categoria'
         })
     }
+  }
+
+  filterCategory(e): void {
+
+    if(e === '') {
+      this._courseService
+        .pagination(1, 100)
+        .subscribe(data => {
+          let courses: any = data
+          this.courses = courses.result.docs
+        }, error => console.log(error))
+
+      return  
+    }
+
+    this._courseService
+    .pagination(1, 100)
+    .subscribe(data => {
+      const courses: any = data
+      this.courses = courses.result.docs.filter(item => item.idCategory.name.trim().toLowerCase() === e.trim().toLowerCase())
+      }, error => console.log(error))
   }
 }
